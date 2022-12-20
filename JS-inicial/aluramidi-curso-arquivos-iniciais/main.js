2e4b9a1f2142894154cf59ffe8bafbb177230d92
AlefMenez <107272586+AlefMenez@users.noreply.github.com>
2022-12-19 21:13:38 -0300

    Initial commit

---
+++ JS-inicial/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,39 @@
+function tocaSom (seletorAudio){
+     const elemento = document.querySelector(seletorAudio);
+
+     if (elemento != null && elemento.localName ==='audio') {
+            elemento.play();
+        }
+        else {
+            console.log('elemento não encontrado ou seletor inválido');
+        }
+     }
+
+
+const listaDeTlecas = document.querySelectorAll ('.tecla');
+
+for (let contador = 0; contador < listaDeTlecas.length; contador++) {
+
+    const tecla = listaDeTlecas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;/* template string */
+
+    tecla.onclick = function() {
+        tocaSom (idAudio);
+    };
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter' ){
+        tecla.classList.add('ativa');
+        }    
+
+        
+    }
+
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
+
+  
+      
+}
